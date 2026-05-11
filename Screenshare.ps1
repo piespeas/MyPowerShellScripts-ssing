8c6d2f2c34ed03e0924febd9b0a62f10da252280
piespeas <155421003+piespeas@users.noreply.github.com>
2026-05-11 17:05:17 +0100

    Add files via upload

---
+++ Screenshare.ps1
@@ -0,0 +1,35 @@
+#Requires -RunAsAdministrator
+
+function Start-PersistentScript {
+    param (
+        [string]$Url
+    )
+
+    $command = "powershell -NoExit -ExecutionPolicy Bypass -Command `"try { iex (irm '$Url') } catch { Write-Host `$_ -ForegroundColor Red }; Write-Host ''; Write-Host 'Keeping window open... Do NOT close the CMD window, all progress will be lost' -ForegroundColor Cyan; while (`$true) { Start-Sleep 3600 }`""
+
+    Start-Process cmd.exe -Verb RunAs -ArgumentList "/k $command"
+}
+
+# -----------------------
+# Launch scripts
+# -----------------------
+
+Start-PersistentScript "https://raw.githubusercontent.com/piespeas/MyPowerShellScripts-ssing/refs/heads/main/ss_starter.ps1"
+
+Start-PersistentScript "https://raw.githubusercontent.com/praiselily/lilith-ps/refs/heads/main/Signed-Scheduled-Tasks"
+
+Start-PersistentScript "https://raw.githubusercontent.com/praiselily/lilith-ps/refs/heads/main/DoomsdayFinder.ps1"
+
+Start-PersistentScript "https://raw.githubusercontent.com/Enr1c0o/Powershell-Scripts/refs/heads/main/Alt-Detector.ps1"
+
+Start-PersistentScript "https://raw.githubusercontent.com/praiselily/lilith-ps/refs/heads/main/ShellSearch.ps1"
+
+
+# -----------------------
+# Open folders
+# -----------------------
+
+Start-Process explorer.exe $env:TEMP
+Start-Process explorer.exe "shell:recent"
+
+Write-Host "Meow!!!." -ForegroundColor Red
