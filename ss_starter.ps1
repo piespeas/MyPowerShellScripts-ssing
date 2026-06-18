2c1ae45d868aba8ff2ba7b22c1f60e97f59ca6c1
piespeas <155421003+piespeas@users.noreply.github.com>
2026-06-18 21:58:17 +0100

    Restore and update Start-PersistentScript function

--- ss_starter.ps1
+++ ss_starter.ps1
@@ -28,8 +28,6 @@ Start-PersistentScript "https://raw.githubusercontent.com/praiselily/lilith-ps/r
 
 Start-PersistentScript "https://raw.githubusercontent.com/PureIntent/ScreenShare/main/RedLotusBam.ps1"
 
-Start-PersistentScript "https://raw.githubusercontent.com/Nickk196/MacroDetector/refs/heads/main/MacroDetector.ps1"
-
 
 
 
